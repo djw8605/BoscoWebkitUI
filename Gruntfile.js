d70a491943875d5d7abe019daa3e28f01c539c6c
Derek Weitzel <djw8605@gmail.com>
2014-07-08 21:08:22 -0500

    Adding shell run for mac.

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,13 +35,21 @@ module.exports = function(grunt) {
         ext: '.js'
       },
     },
+    
+    shell: {
+      start_webkit: {
+        command: 'open webkitbuilds/releases/boscoui/mac/BoscoUI.app'
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-node-webkit-builder');
   grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-shell');
 
   grunt.registerTask('default', ['coffee', 'less', 'nodewebkit']);
+  grunt.registerTask('run', ['coffee', 'less', 'nodewebkit', 'shell:start_webkit'])
 
 
 };
